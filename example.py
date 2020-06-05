536e3367185a13b9a1824c5eaf72583ec02b80ed
ldrbmrtv <baimuratov.i@gmail.com>
2020-06-05 21:15:06 +0300

    Add files via upload

---
+++ example.py
@@ -0,0 +1,25 @@
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.cluster import KMeans
+from sklearn import metrics
+import time
+import prophet
+    
+if __name__ == '__main__':
+
+    n = 25
+    d = 4
+    k = 10
+    
+    data, target = make_blobs(n, d, k)
+    
+    start = time.time()
+    k_inf = prophet.lpic(n)
+    d_time = time.time() - start
+    model = KMeans(n_clusters=k_inf).fit(data)
+    labels = model.labels_
+    sil = metrics.silhouette_score(data, labels)
+    fm = metrics.fowlkes_mallows_score(target, labels)
+    
+    print('Time = {}'.format(d_time))
+    print('Silhouette = {}'.format(sil))
+    print('Fowlkes-Mallows = {}'.format(fm))
---
+++ prophet.py
@@ -0,0 +1,63 @@
+from collections import Counter
+from math import log
+import numpy as np
+from operator import itemgetter
+
+def parts(n):
+    a = [1]*n
+    y = -1
+    v = n
+    while v > 0:
+        v -= 1
+        x = a[v] + 1
+        while y >= 2 * x:
+            a[v] = x
+            y -= x
+            v += 1
+        w = v + 1
+        while x <= y:
+            a[v] = x
+            a[w] = y
+            yield a[:w + 1]
+            x += 1
+            y -= 1
+        a[v] = x + y
+        y = a[v] - 1
+        yield a[:w]
+
+def inf(part, n):
+
+    arr = [log(x, n) for x in part]
+    
+    return np.mean(arr)
+
+def lpic(n, cash = {}):
+
+    file_name = 'mmry_lpic'
+    
+    if cash == {}:
+        f = open(file_name + '.txt', 'r')
+        for line in f.readlines():
+            key, val = line.split(' ')
+            cash[int(key)] = int(val)
+        f.close()
+        
+    k = cash.get(n)
+
+    if k == None:
+
+        arr = []
+        for i in range(1, n+1):
+            arr.append([i, 0])
+
+        _parts = parts(n)
+        for part in _parts:
+            arr[len(part)-1][1] += inf(part, n)
+            
+        arr.sort(key=itemgetter(1), reverse = True)
+        k = arr[0][0]
+        cash[n]=k
+        f = open(file_name + '.txt', 'a+')
+        f.write('{} {}\n'.format(n, k))
+
+    return k
